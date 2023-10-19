8871b242a289272b0e00b6d5425b74dc4785dcb1
shivanshusingh409 <85069068+shivanshusingh409@users.noreply.github.com>
2023-10-19 14:33:20 +0530

    Add files via upload

---
+++ convert_to_lower_case.py
@@ -0,0 +1,8 @@
+print("Hello world")
+print("Enter your name: ")
+name1 = input()
+print("Enter partner's name: ")
+name2 = input()  
+combined_names = name1 + name2
+lower_names = combined_names.lower()
+print(lower_names)
